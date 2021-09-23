e717af47beea34dac15ec72ca15e2a48da50987f
Ivan Bega <bega.ivan03@gmail.com>
2021-09-23 16:17:28 +0300

    Added BSTQueue class

---
+++ lab1/BSTQueue.cpp
@@ -0,0 +1,48 @@
+#pragma once
+#include "Queue.cpp"
+#include <iostream>
+template<class T>
+class BSTQueue : Queue<T>
+{
+public:
+	void push(T data, int priority)
+	{
+		insert(head, data, priority);
+	}
+	T lookup_elem()
+	{
+		return NULL;
+	}
+	T pop()
+	{
+		return NULL;
+	}
+private:
+	struct Tree
+	{
+		T data;
+		int priority;
+		Tree* lt;
+		Tree* rt;
+		Tree(T Data, int Priority, Tree* left = NULL, Tree* right = NULL)
+		{
+			data = Data; priority = Priority; lt = left; rt = right;
+		}
+	};
+	Tree* head = NULL;
+	Tree* insert(Tree* current, T data, int priority)
+	{
+		if (current == NULL)
+		{
+			return new Tree(data, priority);
+		}
+		if (priority < current->priority)
+		{
+			current->lt = insert(current->lt, data, priority);
+		}
+		else
+		{
+			current->rt = insert(current->rt, data, priority);
+		}
+	}
+};
--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -4,15 +4,16 @@
 #include "BinaryHeapQueue.cpp"
 #include "Date.cpp"
 #include "VectorQueue.cpp"
+#include "BSTQueue.cpp"
 #include <iostream>
 
 int main()
 {
-    LinkedListQueue<int> q;
+    BSTQueue<int> q;
     q.push(1, 1);
     q.push(2, 1);
     q.push(3, 1);
+    /*std::cout << q.pop() << std::endl;
     std::cout << q.pop() << std::endl;
-    std::cout << q.pop() << std::endl;
-    std::cout << q.pop() << std::endl;
+    std::cout << q.pop() << std::endl;*/
 }
